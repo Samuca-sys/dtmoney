76a3161f304941d2d64168679424e473f0c53404
Samuel Macedo <samuel.mferreira@hotmail.com>
2021-03-17 09:50:20 -0300

    style: add font style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -33,6 +33,14 @@ html {
     -webkit-font-smoothing: antialiaded//fonte mais nítida
   }
 
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
