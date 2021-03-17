f5cea83e4029f99c61363c49b3cf836c20d1fb97
Samuel Macedo <samuel.mferreira@hotmail.com>
2021-03-17 15:40:54 -0300

    ci:  create mirage server api on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api'; //namespace by /api route and so on
+    this.get('/transaction', () => {//get method by /transaction route
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
+
 
 ReactDOM.render(
   <React.StrictMode>
