8378838273e9c88ba086f8d8ceb06da289d80e92
Samuel Macedo <samuel.mferreira@hotmail.com>
2021-03-18 17:14:32 -0300

    ci: add integration between send & receiving data

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
+import schema from 'miragejs/orm/schema';
 
 createServer({
+  models: {
+    //Fictional table 
+    transaction: Model
+  },
+
   routes() {
     this.namespace = 'api'; //namespace by /api route and so on
+
     this.get('/transactions', () => {//get method by /transactions route list
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+
+      //return all registers from transaction fictional table
+      return this.schema.all('transaction')
     })
+
     //post method by /transactions route list
     //schema = database, request = data
-    this.post('/transactions', (shema, request) => {
+    this.post('/transactions', (schema, request) => {
+
       //data needs to be converted from string to JSON
       const data = JSON.parse(request.requestBody)
 
-      return data;
+      //return fictional table with data inside it
+      return schema.create('transaction', data);
     })
   }
 })
-
-
 
 ReactDOM.render(
   <React.StrictMode>
