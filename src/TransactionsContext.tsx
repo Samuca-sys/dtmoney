fcb1ac9e35761fee381b88644d4bd640b9b94b31
Samuel Macedo <samuel.mferreira@hotmail.com>
2021-03-19 10:26:20 -0300

    ci: add steps to createTransaction in the Context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -12,9 +12,20 @@ interface Transaction {
 
 interface TransactionProviderProps {
   children: ReactNode
-
 }
+//2- First way
+// interface TransactionInput {
+//   title: string,
+//   type: string,
+//   category: string
+//   amount: number,
+// }
 
+//3 - Second way (omitting elements from Transaction interface)
+//type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+//4 - Third way (picking specific elements from Transaction interface)
+type TransactionInput = Pick<Transaction, 'title' | 'type' | 'category' | 'amount'>
 
 export const TransactionsContext = createContext<Transaction[]>([]);
 
@@ -28,6 +39,18 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     api.get('transactions')
       .then(response => setTransactions(response.data.transactions))
   }, []);
+
+  //0 -TransactionsContext create a transaction
+  //(function createTransaction(transaction) {})
+
+  //1 - createTransaction receives another interface 
+  //(function createTransaction(transaction: TransactionInput) {})
+  //or type using the following ways (omit or pick)
+  function createTransaction(transaction: TransactionInput) {
+
+    api.post('/transactions', transaction)//method post route 
+  }
+
 
   return (
     <TransactionsContext.Provider value={transactions}>
